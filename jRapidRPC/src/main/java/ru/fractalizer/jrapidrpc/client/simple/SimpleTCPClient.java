4fbdc3d14114de927a444c2c68acff37082a31ed
Vladislav Rastrusny <FractalizeR@yandex.ru>
2011-06-12 20:58:28 +0400

    isConnected() fixed at client

--- jRapidRPC/src/main/java/ru/fractalizer/jrapidrpc/client/simple/SimpleTCPClient.java
+++ jRapidRPC/src/main/java/ru/fractalizer/jrapidrpc/client/simple/SimpleTCPClient.java
@@ -113,6 +113,6 @@ public class SimpleTCPClient implements InvocationHandler {
      * @return True, if socket is connected, false otherwise
      */
     public boolean isConnected() {
-        return socket != null && socket.isConnected();
+        return (socket != null) && (!socket.isClosed()) && socket.isConnected();
     }
 }
